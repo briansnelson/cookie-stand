62c83c4d5deaa5a258a880e274652559b51a73b4
briansnelson <brian@grit360.com>
2021-05-15 20:16:46 -0700

    created constructor but no tables. render each store

--- js/app.js
+++ js/app.js
@@ -10,7 +10,58 @@ const dubaiList = document.getElementById('dubai-list');
 const parisList = document.getElementById('paris-list');
 const limaList  = document.getElementById('lima-list');
 const mainContainer = document.getElementById('container');
+const cookiesTable = document.querySelector('table');
+let allStores = [];
+console.log(mainContainer);
+
 // console.log(mainContainer);
+
+// Here I am going to try my constructor method. I think each attribute should be the same attributes of the previous formulas.
+function Stores(name, min, max, avg, cookiesPerHourArray){
+  this.name = name;
+  this.min = min;
+  this.max = max;
+  this.avg = avg;
+  this.cookiesPerHourArray = [];
+  this.dailyTotal = 0;
+  allStores.push(this);
+}
+Stores.prototype.getCustomersPerHour = function () {
+  return Math.floor(Math.random() * (this.max - this.min +1) + this.min);
+},
+Stores.prototype.calcCookiesPerHour = function(){
+  for (let i = 0; i <hours.length; i++) {
+    let hourlyCustomer = this.getCustomersPerHour();
+    let hourlyCookiesSold = Math.ceil(hourlyCustomer * this.avg);
+    this.cookiesPerHourArray.push(hourlyCookiesSold);
+    this.dailyTotal += hourlyCookiesSold;
+    console.log(hourlyCookiesSold);
+  }
+},
+Stores.prototype.render = function() {
+  seattle.calcCookiesPerHour();
+  for (let i = 0; i < hours.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = `${hours[i]}: ${this.cookiesPerHourArray[i]} cookies`;
+    seattleList.appendChild(li);
+  }
+}
+let seattleStore = new Stores('Seattle', 23, 65, 6.3,);
+seattleStore.render;
+
+let tokyoStore = new Stores('Tokyo', 3, 24, 1.2);
+tokyoStore.render;
+
+let dubiaStore = new Stores('Dubai', 11, 38, 3.7);
+dubiaStore.render;
+
+let parisStore = new Stores('Paris', 20, 38, 2.3);
+parisStore.render;
+
+let limaStore = new Stores('Lima', 2, 16, 4.6);
+limaStore.render; 
+
+
 
 let seattle = {
   name: 'Seattle',
